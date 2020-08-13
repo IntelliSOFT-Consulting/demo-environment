55580530247edced12d8d57b291004795b6f9064
KelvinWanyama <kevmurumba@gmail.com>
2020-08-13 14:49:32 +0300

    Changed nginxy certificate links

--- nginx-proxy/cmd.sh
+++ nginx-proxy/cmd.sh
@@ -1,10 +1,8 @@
 #/bin/sh
 
 : ${TRUSTCHAIN=""}
-#: ${CERT_LOCATION=/etc/nginx/certs/fullchain.pem}
-#: ${KEY_LOCATION=/etc/nginx/certs/privkey.pem}
-: ${CERT_LOCATION=/etc/ssl/crt/server.crt}
-: ${KEY_LOCATION=/etc/ssl/crt/server.key}
+: ${CERT_LOCATION=/etc/nginx/certs/fullchain.pem}
+: ${KEY_LOCATION=/etc/nginx/certs/privkey.pem}
 : ${SERVER_NAME=localhost}
 : ${DHIS_FWD_PORT=9001}
 : ${OPENEMPI_FWD_PORT=9002}
